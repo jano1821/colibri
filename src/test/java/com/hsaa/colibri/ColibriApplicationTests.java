41d2892332974fb258cf26f9efe96921862aafd2
alejandro nunez <jano18_21@hotmail.com>
2022-01-09 11:15:26 -0500

    primer commit

---
+++ src/test/java/com/hsaa/colibri/ColibriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsaa.colibri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColibriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
